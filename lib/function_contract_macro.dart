757f170e8b870fdb314fe3fb6fbbdc8a1879cb3d
Bence Ladomerszky <bence.ladomerszky@otpbank.hu>
2024-10-29 09:00:02 +0100

    refactor

--- lib/function_contract_macro.dart
+++ lib/function_contract_macro.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:contracts_for_dart/utils/source_span_parser.dart';
 import 'package:macros/macros.dart';
+import 'package:source_span/source_span.dart';
 
 macro class FunctionContract implements FunctionDefinitionMacro{
   const FunctionContract({required this.precondition,required this.postcondition});
@@ -13,13 +14,58 @@ macro class FunctionContract implements FunctionDefinitionMacro{
     final parser = SourceSpanParser(function.library.uri);
 
     final spans = parser.parseFile();
+    final invariant = _findInvariant(spans, function.identifier.name);
+    final (oldVariables,modifiedPostcondition) = _findOldVariables(postcondition);
+    final (originalfunctionBody,returnUsedInPostcondition) = _modifyOriginalFunctionBody(spans, function.identifier.name, postcondition, invariant ?? "", oldVariables ?? "");
+  
+    
+    builder.augment(FunctionBodyCode.fromParts(["{\n "+
+    "${invariant != "" ? "if(!($invariant)){ \n "+
+    "   throw ('Invariant failed, $invariant');\n"+
+    "}" : ""} \n "+
+    "${oldVariables ?? ""} \n "+
+    "if(!(${precondition == "" ? true : precondition})){ \n "+
+    "   throw ('Precondition failed, $precondition'); \n "+
+    "} \n $originalfunctionBody \n "+
+    "${!returnUsedInPostcondition ? "\n ${invariant != "" ? "if(!($invariant)){ \n "+
+    "throw ('Invariant failed, $invariant');}" : ""} \n"+
+    " if(!(${modifiedPostcondition == "" ? "true" : modifiedPostcondition})){ "+
+    "\n throw 'Postcondition failed, $postcondition';}\n}" : ""}"
+    ]));
+  }
+}
 
-    var cc = false;
-    var invariant = "";
-    var counter = 0;
-    bool inClass = false;
+(String,bool) _modifyOriginalFunctionBody(Iterable<SourceSpan> spans, String functionName, String postcondition, String invariant, String modifiedPostcondition){
+bool found = false;
+    bool returnUsed= false;
+    String originalfunctionBody = "";
+    for (final span in spans) {
+      if(found && !span.text.contains("}")){
+        if(span.text.contains("return") && postcondition.contains("ret")){
+          returnUsed = true;
+          originalfunctionBody += "\n var ret = ${span.text.replaceFirst("return", "").trim()}";
+          originalfunctionBody += "\n ${invariant != "" ? 
+            "if(!($invariant)){throw ('Invariant failed, $invariant');}" : ""} " + 
+            "\n if(!(${modifiedPostcondition == "" ? true : modifiedPostcondition})){ \n throw 'Postcondition failed, $postcondition';}\n";
+        }
+        originalfunctionBody += span.text + "\n";
+        if(span.text.contains("return")){
+          originalfunctionBody += "}";
+        }
+      }
+      if(found && span.text.contains("}")){
+        break;
+      }
+      if(span.text.contains("$functionName(")){
+        found = true;
+      }
+    }
+    return (originalfunctionBody,returnUsed);
+}
 
+(String?,String) _findOldVariables(String postcondition){
     final variablesToSave = <String>[];
+    var addToBody;
     if(postcondition.contains("old")){
       RegExp exp = RegExp(r'old\([A-Z-a-z-0-9-_]*\)');
       final matches = exp.allMatches(postcondition);
@@ -32,7 +78,7 @@ macro class FunctionContract implements FunctionDefinitionMacro{
         }
       }
     }
-    var addToBody = "";
+
     var modifiedPostcondition = postcondition;
     if(variablesToSave.isNotEmpty){
       for(final variable in variablesToSave){
@@ -40,9 +86,17 @@ macro class FunctionContract implements FunctionDefinitionMacro{
         addToBody += "\n final _$variable = $variable; \n";
       }
     }
+    return (addToBody,modifiedPostcondition);
+}
 
-    for(final span in spans){
-      if(inClass && cc && span.text.contains("${function.identifier.name}(")){
+String? _findInvariant(Iterable<SourceSpan> spans, String functionName){
+    var cc = false;
+    var invariant;
+    var counter = 0;
+    bool inClass = false;
+
+      for(final span in spans){
+      if(inClass && cc && span.text.contains("${functionName}(")){
         break;
       }
       if(inClass && span.text.contains("{")){
@@ -65,33 +119,8 @@ macro class FunctionContract implements FunctionDefinitionMacro{
         invariant = span.text.substring(span.text.indexOf("\"")+1, span.text.lastIndexOf("\""));
       }
     }
-  
-    bool found = false;
-    bool returnUsed= false;
-    String originalfunctionBody = "";
-    for (final span in spans) {
-      if(found && !span.text.contains("}")){
-        if(span.text.contains("return")){
-          returnUsed = true;
-          originalfunctionBody += "\n var ret = ${span.text.replaceFirst("return", "").trim()}";
-          originalfunctionBody += "\n \n ${invariant != "" ? "if(!($invariant)){throw ('Invariant failed, $invariant');}" : ""} \n if(!(${modifiedPostcondition == "" ? true : modifiedPostcondition})){ \n throw 'Postcondition failed, $postcondition';}\n";
-        }
-        originalfunctionBody += span.text + "\n";
-        if(span.text.contains("return")){
-          originalfunctionBody += "}";
-        }
-      }
-      if(found && span.text.contains("}")){
-        break;
-      }
-      if(span.text.contains(function.identifier.name)){
-        found = true;
-      }
-    }
-    builder.augment(FunctionBodyCode.fromParts([
-      "{\n ${invariant != "" ? "if(!($invariant)){throw ('Invariant failed, $invariant');}" : ""} \n $addToBody \n if(!(${precondition == "" ? true : precondition})){ \n throw ('Precondition failed, $precondition');} \n $originalfunctionBody ${!returnUsed ? "\n \n ${invariant != "" ? "if(!($invariant)){throw ('Invariant failed, $invariant');}" : ""} \n if(!(${modifiedPostcondition == "" ? "true" : modifiedPostcondition})){ \n throw 'Postcondition failed, $postcondition';}\n}" : ""}"
-    ]));
-  }
+
+    return invariant;
 }
 
 
